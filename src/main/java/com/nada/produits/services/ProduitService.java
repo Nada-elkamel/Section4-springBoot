7c0ae6f32724f50cafc41d2ae0d0502c8899409f
Nada El Kamel <114959945+Nada-elkamel@users.noreply.github.com>
2023-03-20 13:07:54 +0100

    Add files via upload

---
+++ src/main/java/com/nada/produits/services/ProduitService.java
@@ -0,0 +1,15 @@
+package com.nada.produits.services;
+
+import java.util.List;
+
+import com.nada.produits.entities.Produit;
+
+public interface ProduitService {
+
+	Produit saveProduit(Produit p);
+	Produit updateProduit(Produit p);
+	void deleteProduit(Produit p);
+	void deleteProduitById(Long id);
+	Produit getProduit(Long id);
+	List<Produit> getAllProduits();
+}
